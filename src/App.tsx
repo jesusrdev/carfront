1d5b010fd5d178f3b8f7ed9f00d706ebe1f73c52
jesusrdev <102635058+jesusrdev@users.noreply.github.com>
2024-09-25 10:53:52 -0500

    deleteing default styles and add mui components

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,17 @@
-import './App.css'
+import { AppBar, Container, CssBaseline, Toolbar, Typography } from "@mui/material"
 
 function App() {
   return (
-    <>
-    </>
+    <Container maxWidth="xl">
+      <CssBaseline />
+      <AppBar position="static">
+        <Toolbar>
+          <Typography variant="h6">
+            Car Shop
+          </Typography>
+        </Toolbar>
+      </AppBar>
+    </Container>
   )
 }
 
