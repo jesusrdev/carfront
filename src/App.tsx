c240db02e529798cdd7bd3ec1f2e9f3bfbe7f0c9
jesusrdev <102635058+jesusrdev@users.noreply.github.com>
2024-09-25 11:07:11 -0500

    adding a dar theme

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,38 @@
-import { AppBar, Container, CssBaseline, Toolbar, Typography } from "@mui/material"
+import {
+  AppBar,
+  Container,
+  createTheme,
+  CssBaseline,
+  ThemeProvider,
+  Toolbar,
+  Typography,
+} from "@mui/material";
+
+const darkTheme = createTheme({
+  // palette: {
+  //   mode: "dark",
+  //   primary: {
+  //     main: '#ff5252',
+  //   },
+  // },
+  colorSchemes: {
+    dark: true,
+  },
+});
 
 function App() {
   return (
-    <Container maxWidth="xl">
-      <CssBaseline />
-      <AppBar position="static">
-        <Toolbar>
-          <Typography variant="h6">
-            Car Shop
-          </Typography>
-        </Toolbar>
-      </AppBar>
-    </Container>
-  )
+    <ThemeProvider theme={darkTheme}>
+      <Container maxWidth="xl">
+        <CssBaseline />
+        <AppBar position="static">
+          <Toolbar>
+            <Typography variant="h6">Car Shop</Typography>
+          </Toolbar>
+        </AppBar>
+      </Container>
+    </ThemeProvider>
+  );
 }
 
-export default App
+export default App;
