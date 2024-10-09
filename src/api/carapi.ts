1a27cb56b6235885e93c994ffd8399715e99a79e
jesusrdev <102635058+jesusrdev@users.noreply.github.com>
2024-10-08 22:38:40 -0500

    refactor the get token in headers

--- src/api/carapi.ts
+++ src/api/carapi.ts
@@ -1,48 +1,43 @@
-import axios from "axios";
+import axios, { AxiosRequestConfig } from "axios";
 import { Car, CarEntry, CarResponse } from "../types";
 
-export const getCars = async (): Promise<CarResponse[]> => {
+const getAxiosConfig = (): AxiosRequestConfig => {
   const token = sessionStorage.getItem("jwt");
-  const response = await axios.get(`${import.meta.env.VITE_API_URL}/api/cars`, {
+  return {
     headers: {
       Authorization: token,
+      "Content-Type": "application/json",
     },
-  });
+  };
+};
+
+export const getCars = async (): Promise<CarResponse[]> => {
+  const response = await axios.get(
+    `${import.meta.env.VITE_API_URL}/api/cars`,
+    getAxiosConfig()
+  );
   return response.data._embedded.cars;
 };
 
 export const deleteCar = async (link: string): Promise<CarResponse> => {
-  const token = sessionStorage.getItem("jwt");
-  const response = await axios.delete(link, {
-    headers: {
-      Authorization: token,
-    },
-  });
+  const response = await axios.delete(link, getAxiosConfig());
   return response.data;
 };
 
 export const addCar = async (car: Car): Promise<CarResponse> => {
-  const token = sessionStorage.getItem("jwt");
   const response = await axios.post(
     `${import.meta.env.VITE_API_URL}/api/cars`,
     car,
-    {
-      headers: {
-        "Content-Type": "application/json",
-        Authorization: token,
-      },
-    }
+    getAxiosConfig()
   );
   return response.data;
 };
 
 export const updateCar = async (carEntry: CarEntry): Promise<CarResponse> => {
-  const token = sessionStorage.getItem("jwt");
-  const response = await axios.put(carEntry.url, carEntry.car, {
-    headers: {
-      "Content-Type": "application/json",
-      Authorization: token,
-    },
-  });
+  const response = await axios.put(
+    carEntry.url,
+    carEntry.car,
+    getAxiosConfig()
+  );
   return response.data;
 };
